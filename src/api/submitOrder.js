07e2b550533e4931410e8e73f031716f7bf984f4
Vathsan07 <40523831+Vathsan07@users.noreply.github.com>
2025-11-16 19:17:29 +0530

    Webhook url update

--- src/api/submitOrder.js
+++ src/api/submitOrder.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const response = await axios.post('https://hook.us2.make.com/fyext0qe7r45x69j2bkq6q9ic8qb26ta', req.body);
+      const response = await axios.post('https://hook.us2.make.com/ox8dtbe946ulga5g5b7b3p28mnt31ml7', req.body);
       res.status(200).json({ success: true, data: response.data });
     } catch (error) {
       console.error('Error:', error);
